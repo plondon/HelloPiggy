4b95719ef3c5a99230fae83a2e2361a0c27f6373
plondon <philip@blockchain.com>
2017-11-19 17:43:29 -0500

    Start working on getting txs

--- src/components/Plaid.js
+++ src/components/Plaid.js
@@ -4,6 +4,7 @@ import PlaidAuthenticator from 'react-native-plaid-link';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 
 var authorize = 'http://localhost:8000/auth';
+var getTransactions = 'http://localhost:8000/get_txs';
 var getAccessToken = 'http://localhost:8000/get_access_token';
 
 export default class Plaid extends React.Component {
@@ -32,28 +33,39 @@ export default class Plaid extends React.Component {
     });
   }
   
+  getTransactions = () => {
+    this.setState({ activity: true });
+    axios({ method: 'post', url: getTransactions,
+      params: {
+        access_token: 1
+      }
+    }).then((res) => { debugger; this.setState({ access: true, data: res }) })
+  }
+
   render () {
     return this.state.access ? this.renderDetails() : this.renderLogin();
   }
   
   renderDetails () {
-    const { accounts } = this.state.data;
+    // const { accounts } = this.state.data;
 
-    const accountsView = accounts.map((account) => {
-      return (
-        <View key={account.account_id}>
-          <Text>{account.balances.available}</Text>
-        </View>
-      );
-    });
+    // const accountsView = accounts.map((account) => {
+    //   return (
+    //     <View key={account.account_id}>
+    //       <Text>{account.balances.available}</Text>
+    //     </View>
+    //   );
+    // });
     
-    console.log(accounts);
+    console.log(this.state.data);
     
-    return (
-      <View>
-        {accountsView}
-      </View>
-    )
+    return null;
+    
+    // return (
+    //   <View>
+    //     {accountsView}
+    //   </View>
+    // )
   }
   
   renderLogin () {
