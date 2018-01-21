c8855c3c3ca5a1a54ef8c8caedd8a267297d0a91
plondon <philip@blockchain.com>
2018-01-21 15:24:03 -0500

    Facebook and Google continue buttons

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { checkActiveUser, handleFacebookLogin, handleGoogleLogin } from '../actions'
-import { ActivityIndicator, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import { ActivityIndicator, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
 
 class Auth extends React.Component {
   componentDidMount () {
@@ -31,13 +31,19 @@ class Auth extends React.Component {
           </View>
           <View style={styles.centering}>
             <TouchableHighlight style={styles.facebookButton}>
-              <Text style={styles.facebookButtonText} onPress={() => this.handleFacebookLogin()}>Login with Facebook</Text>
+              <View style={styles.loginButtonView}>
+                <Image source={require('../img/facebook.png')} style={{width: 16, height: 16, margin: 6}} />
+                <Text style={styles.loginButtonText} onPress={() => this.handleFacebookLogin()}>Continue with Facebook</Text>
+              </View>
             </TouchableHighlight>
             <View style={styles.orView}>
               <Text style={styles.or}>Or</Text>
             </View>
-            <TouchableHighlight>
-              <Text onPress={() => this.handleGoogleLogin()}>Login with Google+</Text>
+            <TouchableHighlight style={styles.googleButton}>
+              <View style={styles.loginButtonView}>
+                <Image source={require('../img/google.png')} style={{width: 16, height: 16, margin: 6}} />
+                <Text style={styles.loginButtonText} onPress={() => this.handleGoogleLogin()}>Continue with Google</Text>
+              </View>
             </TouchableHighlight>
           </View>
         </View>
@@ -67,15 +73,22 @@ const styles = StyleSheet.create({
     color: '#F8F7F5'
   },
   'facebookButton': {
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingRight: 7,
-    paddingLeft: 7,
     borderRadius: 2,
     backgroundColor: '#4267b2'
   },
-  'facebookButtonText': {
+  'googleButton': {
+    borderRadius: 2,
+    backgroundColor: '#dd4b39'
+  },
+  'loginButtonView': {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  'loginButtonText': {
     fontSize: 14,
+    paddingRight: 20,
+    paddingLeft: 6,
     color: '#FFFFFF'
   },
   'orView': {
